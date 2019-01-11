85dba409820380bc7561d087c8bcbd452a60b687
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-01-11 16:52:44 -0500

    read into fembase element and geometry

--- src/read_into_fembase.jl
+++ src/read_into_fembase.jl
@@ -1,3 +1,17 @@
-function gmsh_read_mesh()
+using Base.Threads
 
+function gmsh_read_mesh(fname::AbstractString)
+    meshraw = read_gmsh_ascii(fname)
+    nels = length(meshraw["Elements"]["tags"])
+    els = Vector{Element}(undef, nels)
+    @inbounds @threads for i = 1: nels
+        eltype = GmshCode2FEMBaseType[meshraw["Elements"]["typeEles"][i]]
+        elverts = meshraw["Elements"]["numVerts"][i]
+        els[i] = Element(eltype, elverts)
+    end
+    nodes = meshraw["Nodes"]
+    nnodes = length(nodes["tags"])
+    geometry = [nodes["tags"][i] => [nodes["xs"][i], nodes["ys"][i], nodes["zs"][i]] for i in 1: nnodes] |> Dict
+    update!(els, "geometry", geometry)
+    return els
 end
