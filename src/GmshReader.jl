39dfc963568e1f6f78184e92101c05e695c12dfb
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-05-08 18:57:21 -0400

    fix

--- src/GmshReader.jl
+++ src/GmshReader.jl
@@ -2,13 +2,14 @@ module GmshReader
 
 using Libdl
 
-const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
+const deps_path = joinpath(@__DIR__, "..", "deps")
 
 if !isfile(depsjl_path)
     error("Gmsh SDK not installed properly, run Pkg.build(\"GmshReader\"), restart Julia and try again")
 end
 
-include(depsjl_path)
+include(joinpath(deps_path, "deps.jl"))
+include(joinpath(deps_path, "usr/lib/gmsh.jl"))
 
 include("read_gmsh_ascii.jl")
 
