168810ac7c4644b6b7ffdd397a816e70f127b3fe
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-05-08 22:12:34 -0400

    try to use original lib

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,5 @@
 using BinaryProvider
+using SHA
 
 const verbose = "--verbose" in ARGS
 const prefix = Prefix(get([a for a in ARGS if a != "--verbose"], 1, joinpath(@__DIR__, "usr")))
@@ -13,8 +14,8 @@ version = "4.3.0"
 download_info = Dict(
     Linux(:i686, :glibc) => ("$bin_prefix/Linux/gmsh-$version-Linux32-sdk.tgz", "703c997f63177a3b15e72dca63d3db46a575d4cb7e41ca860f3d27623b0efed7"),
     Linux(:x86_64, :glibc) => ("$bin_prefix/Linux/gmsh-$version-Linux64-sdk.tgz", "3cee6e84e6e2f9cfb69b9041d65db58ba0eb4b5aa7e6fcc72fd0485d40e46fe6"),
-    Windows(:i686) => ("$bin_prefix/Windows/gmsh-$version-Windows32-sdk.tgz", "88b6d5d3aa77ee45514731aa8a105a070807a703166f187e41229277fc0c53f4"),
-    Windows(:x86_64) => ("$bin_prefix/Windows/gmsh-$version-Windows64-sdk.tgz", "632f208d00fbb42204358a92f2bc9af1eb38cf98732ff4bbf206f3a6f7193677"),
+    Windows(:i686) => ("$bin_prefix/Windows/gmsh-$version-Windows32-sdk.zip", "88b6d5d3aa77ee45514731aa8a105a070807a703166f187e41229277fc0c53f4"),
+    Windows(:x86_64) => ("$bin_prefix/Windows/gmsh-$version-Windows64-sdk.zip", "632f208d00fbb42204358a92f2bc9af1eb38cf98732ff4bbf206f3a6f7193677"),
     MacOS(:x86_64) => ("$bin_prefix/MacOSX/gmsh-$version-MacOSX-sdk.tgz", "5342ca82a1cae0734fdf189bb98fbc58199ed4e462328b514ad04dbef356b715"),
 )
 
@@ -22,14 +23,31 @@ if any(!satisfied(p; verbose=verbose) for p in products)
     try
         # Download and install binaries
         url, tarball_hash = choose_download(download_info)
-        install(url, tarball_hash; prefix=prefix, force=true, verbose=true)
+        download_path = joinpath(prefix, "download")
+        target_path = joinpath(download_path, basename(url))
+        mkpath(download_path)
+        download(url, target_path)
 
-        content_path = joinpath(prefix, splitext(basename(url))[1])
-        foreach(
-            (x) -> mv(joinpath(content_path, x), joinpath(prefix, x); force=true),
-            readdir(content_path)
-            )
-        rm(content_path; force=true, recursive=true)
+        hash = open(target_path, "r") do f
+            bytes2hex(sha256(f))
+        end
+        hash != tarball_hash && error("Downloaded file $(basename(url)) damaged, please retry.")
+
+        ext = splitext(url)[2]
+        if ext == ".tgz"
+            run(pipeline(`tar zxf $target_path --strip=1 --directory=$(prefix.path)`))
+        elseif ext == ".zip"
+            # unzip cannot strip the top level directory
+            run(pipeline(`unzip $target_path -d $(prefix.path)`))
+            content_path = joinpath(prefix, splitext(basename(url))[1])
+            foreach(
+                (x) -> mv(joinpath(content_path, x), joinpath(prefix, x); force=true),
+                readdir(content_path)
+                )
+            rm(content_path; force=true, recursive=true)
+        else
+            error("Extname $ext not implemented.")
+        end
     catch e
         if typeof(e) <: ArgumentError
             error("Your platform $(Sys.MACHINE) is not supported by this package!")
--- src/GmshReader.jl
+++ src/GmshReader.jl
@@ -2,13 +2,16 @@ module GmshReader
 
 using Libdl
 
-const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
+const deps_path = joinpath(@__DIR__, "..", "deps")
+const depsjl = joinpath(deps_path, "deps.jl")
 
-if !isfile(depsjl_path)
+if !isfile(depsjl)
     error("Gmsh SDK not installed properly, run Pkg.build(\"GmshReader\"), restart Julia and try again")
 end
 
-include(depsjl_path)
+include(depsjl)
+include(joinpath(deps_path, "usr", "lib", "gmsh.jl"))
+export gmsh
 
 include("read_gmsh_ascii.jl")
 
