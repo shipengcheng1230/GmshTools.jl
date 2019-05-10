ee3309b4356133aef8dc28e82bb62ed0330838ac
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-05-10 13:43:22 -0400

    remove code shipping

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,11 @@
 using GmshTools
 using Test
 
-demos = ["t1.jl", "t2.jl", "t3.jl", "t4.jl", "t16.jl"]
-
-sample_dir = joinpath(@__DIR__, "samples")
-isdir(sample_dir) || mkdir(sample_dir)
-
-function test_api(x)
-    include(joinpath(@__DIR__, x))
-    mshname = splitext(x)[1] * ".msh"
-    mv(joinpath(@__DIR__, mshname), joinpath(sample_dir, mshname); force=true)
-end
-
 try
-    foreach(test_api, demos)
+    gmsh.initialize()
     @test true
 catch
     @test false
+finally
+    gmsh.finalize()
 end
-
-@test @gmsh_open joinpath(sample_dir, "t1.msh") begin
-    gmsh.model.getDimension()
-end == 2
