c75eaafe21a1be143966672c8d4360610863f2de
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-05-09 23:09:10 -0400

    test

--- src/GmshTools.jl
+++ src/GmshTools.jl
@@ -1,9 +1,14 @@
 module GmshTools
 
+__precompile__(false)
+
 using Libdl
 
 const gmshmodule = joinpath(@__DIR__, "..", "deps", "usr", "lib", "gmsh.jl")
 Base.include(Main, gmshmodule)
+
+function __init__()
+end
 
 export @gmsh_do, @gmsh_open
 
