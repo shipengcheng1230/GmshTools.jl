3565be95ee73418bd50463c9edc2a28424e5d9f9
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-12-27 13:30:31 -0500

    handle non-loadable system

--- src/GmshTools.jl
+++ src/GmshTools.jl
@@ -3,16 +3,20 @@ module GmshTools
 using MLStyle
 
 const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
-if !isfile(depsjl_path)
-    error("libgmsh not installed properly, run Pkg.build(\"GmshTools\"), restart Julia and try again")
-end
-include(depsjl_path)
-
-gmshmodule = joinpath(dirname(libgmsh), "gmsh.jl")
-include(gmshmodule)
 
 function __init__()
-    check_deps()
+    try
+        include(depsjl_path)
+        gmshmodule = joinpath(dirname(libgmsh), "gmsh.jl")
+        include(gmshmodule)
+        Base.invokelatest(check_deps) # world age problem
+    catch ex
+        if isa(ex, ErrorException)
+            @error "libgmsh not installed properly. Please check *.travis* for additional dependencies and rebuild this package."
+        else
+            rethrow(ex)
+        end
+    end
 end
 
 export gmsh, @gmsh_do, @gmsh_open
